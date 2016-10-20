6c5f02ddb505db4674179ec2695f3be7a0d050f1
Leif Olsen <leif.o.olsen@gmail.com>
2016-10-21 00:54:41 +0200

    Updated webpack.config to webpack-2.1.0-beta.25 syntax. Using "rules" instead of "preLoaders/loaders"

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -75,7 +75,7 @@ module.exports = env => {
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          use: ['url-loader?limit=100000&minetype=application/font-woff']
+          use: ['url-loader?limit=100000&mimetype=application/font-woff']
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
