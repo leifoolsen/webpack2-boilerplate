44c5221108a20c28dd3d454a2d04d4897611d794
Leif Olsen <leif.o.olsen@gmail.com>
2016-12-16 00:22:54 +0100

    Client to server logging, WIP

--- src/index.js
+++ src/index.js
@@ -1,34 +1,54 @@
 // Polyfills fetch, generator, ++
 import 'babel-polyfill';
+import 'whatwg-fetch';
 
 import run from './app/app';
+import request from './utils/request';
 
 import './styles.scss';
 
 
+const serverLog = msg => {
+
+  request('/api/log'  ,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: msg,
+      })
+    })
+    .catch(err => err)
+    .then(response => {
+      console.log('**** response', response);
+    });
+
+};
+
 // Unhandled errors should be sent to the server
-
-/*
-window.addEventListener('error', (e) => {
-  //const stack = e.error.stack;
-  //const message = e.error.toString();
-  console.log('XXXXX', e.error, e.colno, e.filename, e.lineno, e.message, e);
-
-  //e.stopPropagation();
-  //e.preventDefault();
-  return false;
-});
-*/
-
 
 if (window && !window.onerror) {
   window.onerror = function (msg, url, lineNo, columnNo, error) {
 
     console.log('#####', msg, url, lineNo, columnNo, error);
+    serverLog(msg);
     return false;
   };
 }
 
+/*
+ window.addEventListener('error', (e) => {
+ //const stack = e.error.stack;
+ //const message = e.error.toString();
+ console.log('XXXXX', e.error, e.colno, e.filename, e.lineno, e.message, e);
+
+ //e.stopPropagation();
+ //e.preventDefault();
+ return false;
+ });
+ */
 
 if (module.hot) {
   // This tells Webpack that this file and all of its dependencies can be replaced.
