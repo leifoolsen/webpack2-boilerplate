a2376c3c390728912984404c498cfbb9ba9b429a
Leif Oddvar Olsen <LeifOddvar.Olsen@skatteetaten.no>
2017-02-09 13:55:49 +0100

    jsdom.init: Added browser language mock

--- test/jsdom-init.js
+++ test/jsdom-init.js
@@ -1,8 +1,9 @@
-// See: https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
-// See: http://www.asbjornenge.com/wwc/testing_react_components.html
-// See: https://github.com/rstacruz/jsdom-global
-// See: https://github.com/dmatteo/jsdomify
-//
+/**
+ * See: https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+ * See: http://www.asbjornenge.com/wwc/testing_react_components.html
+ * See: https://github.com/rstacruz/jsdom-global
+ * See: https://github.com/dmatteo/jsdomify
+ */
 
 const exposedProperties = ['window', 'navigator', 'document'];
 const defaultHtml = '<!doctype html><html><head><meta charset="utf-8"></head><body></body></html>';
@@ -40,36 +41,49 @@ function setupJsDom(markup = defaultHtml, options={}) {
 
   //console.log('Setting up jsdom');
 
-  //const document = jsdom.jsdom(markup, {
+  //const doc = jsdom.jsdom(markup, {
   //  url: 'http://localhost:12345/',
   //  virtualConsole: jsdom.createVirtualConsole().sendTo(console)
   //});
-  const document = jsdom.jsdom(markup, opts);
+  const doc = jsdom.jsdom(markup, opts);
 
-  const window = document.defaultView;
-
-  Object.keys(document.defaultView).forEach( key => {
+  Object.keys(doc.defaultView).forEach( key => {
     if (typeof global[key] === 'undefined') {
       exposedProperties.push(key);
-      global[key] = document.defaultView[key];
+      global[key] = doc.defaultView[key];
     }
   });
 
-  global.document = document;
-  global.window = window;
+  global.document = doc;
+  global.window = doc.defaultView;
   window.console = global.console;
   window.basePath = '/';
   global.navigator = {
     userAgent: 'node.js',
   };
-  document.destroy = teardownJsDom;
-
 
   global.localStorage = global.window.localStorage = storageMock();
   global.sessionStorage = global.window.sessionStorage = storageMock();
 
+  const browserLocale = () => {
+    // http://stackoverflow.com/questions/1043339/javascript-for-detecting-browser-language-preference
+    return navigator.languages
+      ? navigator.languages[0]
+      : navigator.language || navigator.userLanguage;
+  };
+
+  if(!browserLocale()) {
+    Object.defineProperty(navigator, 'language', {
+      writable: false,
+      value: 'en-US',
+    });
+  }
+
+  window.onerror = () => console.log(arguments);
+
   // ... add whatever browser globals your tests might need ...
 
+  document.destroy = teardownJsDom;
 }
 
 function teardownJsDom() {
