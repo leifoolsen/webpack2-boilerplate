c75fab9dddfe5de8d6812b50a98bd6ead4faf92a
Leif Olsen <leif.o.olsen@gmail.com>
2017-02-26 19:42:10 +0100

    Added deepMerge function. Both Object.assign and Object spread only do a shallow merge.

---
+++ src/utils/deep-merge.js
@@ -0,0 +1,56 @@
+/*
+ * Deep merge objects
+ */
+
+/**
+ * Simple is object check.
+ * @param x the object to check
+ * @return true if x is an object, otherwise false
+ * @see  http://reactivex.io/rxjs/file/es6/util/isObject.js.html
+ */
+const isObject = x => x != null && typeof x === 'object';
+
+/**
+ * Deep merge objects
+ * Both Object.assign and Object spread only do a shallow merge.
+ * @param {Object} target
+ * @param {...Object} sources the objects to merge into terget
+ * @return {Object} the merged object
+ * @see http://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+ *
+ * @example
+ * // Given
+ * const x = { a: { a: 1 } }
+ * const y = { a: { b: 1 } }
+ * const z = { ...x, ...y }
+ * // Returns a shallow merge { a: { b: 1 } }
+ * const m = deepMerge(x, y)
+ * // Rerurns a deep merge { a: { a: 1, b: 1 } }
+ */
+
+const deepMerge = function(target, ...sources) {
+
+  const result = Object.assign({}, target);
+
+  if(sources.length > 0) {
+    const source = sources.shift();
+
+    for (let key of Object.keys(source)) {
+      if (isObject(source[key])) {
+        if (!(key in result)) {
+          Object.assign(result, {[key]: source[key]});
+        }
+        else {
+          result[key] = deepMerge(result[key], source[key]);
+        }
+      }
+      else {
+        Object.assign(result, {[key]: source[key]});
+      }
+    }
+    return deepMerge(result, ...sources);
+  }
+  return result;
+};
+
+export default deepMerge;
---
+++ test/utils/deep-merge.spec.js
@@ -0,0 +1,59 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import deepMerge from '../../src/utils/deep-merge';
+
+describe('deep-merge', () => {
+
+  it('should shallow merge two objects', () => {
+    const x = { a: 2 };
+    const y = { a: 1, b: 3 };
+    const z = { ...x, ...y };
+    const expected = {a: 1, b: 3};
+
+    const m = deepMerge(x, y);
+    assert.deepEqual(m, expected);
+    assert.deepEqual(m, z);
+  });
+
+  it('should assert that Object.assign is shallow', () => {
+    const x = { a: { a: 1 } };
+    const y = { a: { b: 1 } };
+    const z = { ...x, ...y };
+    const expected = { a: { a: 1, b: 1 } };
+    assert.notDeepEqual(z, expected);
+  });
+
+  it('should deep merge two objects', () => {
+    const x = { a: { a: 1 } };
+    const y = { a: { b: 1 } };
+    const expected = { a: { a: 1, b: 1 } };
+
+    const m = deepMerge(x, y);
+    assert.deepEqual(m, expected);
+  });
+
+  it('should deep merge three objects', () => {
+    const x = { a: { a: 1 } };
+    const y = { a: { b: 1 } };
+    const z = { a: { c: 1 } };
+    const expected = { a: { a: 1, b: 1, c: 1 } };
+
+    const m = deepMerge(x, y, z);
+    assert.deepEqual(m, expected);
+  });
+
+  it('should be immutable', () => {
+    const x = { a: { a: 1 } };
+    const y = { a: { b: 1 } };
+    const z = { a: { c: 1 } };
+    const expectedX = { a: { a: 1 } };
+    const expectedY = { a: { b: 1 } };
+    const expectedZ = { a: { c: 1 } };
+
+    deepMerge(x, y, z);
+    assert.deepEqual(x, expectedX);
+    assert.deepEqual(y, expectedY);
+    assert.deepEqual(z, expectedZ);
+  });
+
+});
