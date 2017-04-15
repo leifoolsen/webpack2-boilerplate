0490a3378c0c7a2f9bb91acd16f1612764d3cfe0
Leif Olsen <leif.o.olsen@gmail.com>
2017-04-15 12:08:13 +0200

    Renamed paramater x to val

--- src/utils/is-object.js
+++ src/utils/is-object.js
@@ -1,8 +1,8 @@
 /**
  * Simple is object check.
- * @param x the object to check
- * @return true if x is an object, otherwise false
+ * @param val the object to check
+ * @return true if val is an object, otherwise false
  * @see  http://reactivex.io/rxjs/file/es6/util/isObject.js.html
  */
-const isObject = x => x != null && typeof x === 'object';
+const isObject = val => val != null && typeof val === 'object';
 export default isObject;
