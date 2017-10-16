7c7b4712743c91d0b359a651d47f2e5612a0dd16
Leif Olsen <leif.o.olsen@gmail.com>
2017-10-16 23:34:01 +0200

    Added normalize.css as dependency

--- src/vendor.js
+++ src/vendor.js
@@ -1,7 +1,7 @@
 // Import vendor css
 import 'material-design-icons/iconfont/material-icons.css';
 import 'roboto-fontface/css/roboto/sass/roboto-fontface-light.scss';
-import './stylesheets/vendor/_normalize.scss';
+import 'normalize.css/normalize.css';
 
 // Import vendor scripts which are not dynamically imported
 // E.g. the moment package is dynamically imported, see "ping.js"
