bb6886261be5ddde39677567492d321cef12e6b3
Leif Olsen <leif.o.olsen@gmail.com>
2017-11-10 00:21:56 +0100

    Added parseURI utility function

---
+++ src/utils/parse-uri.js
@@ -0,0 +1,37 @@
+/**
+ * Parse URI
+ *  The following are two example URIs and their component parts:
+ *
+ *   http://example.com:8042/over/there?name=ferret#nose
+ *   \__/   \______________/\_________/ \_________/ \__/
+ *   |           |            |            |        |
+ *   scheme    authority     path        query   fragment
+ *   |   _____________________|__
+ *   / \ /                        \
+ *   urn:example:animal:ferret:nose
+ *
+ * @see https://stackoverflow.com/questions/6168260/how-to-parse-a-uri
+ * @see http://www.ietf.org/rfc/rfc3986.txt
+ * @param uri The uri to parse
+ * @return {{scheme: string, host: string, port: string, authority: string, path: string, query: string, fragment: string}}
+ * @author Leif Olsen
+ * @example
+ * // parseURI("http://www.somesite.com/web/page?a=1&b=2"))
+ * // returns
+ */
+const parseURI = uri => {
+  const pattern = new RegExp('^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?');
+  const matches = uri.match(pattern);
+  const [ host = undefined, port = undefined ] = matches[4] ? matches[4].split(':') : [undefined, undefined]; // authority, e.g. localhost:80
+  return {
+    scheme: matches[2],
+    host: host,
+    port: port,
+    authority: matches[4],
+    path: matches[5],
+    query: matches[7],
+    fragment: matches[9]
+  };
+};
+
+export default parseURI;
---
+++ test/unit/utils/parse-uri.spec.js
@@ -0,0 +1,90 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import parseURI from '../../../src/utils/parse-uri';
+
+describe('parse-uri', () => {
+
+  it('should parse "http://example.com:8042/over/there?name=ferret#nose" correctly', () => {
+    const result = parseURI('http://example.com:8042/over/there?name=ferret#nose');
+    const expectedResult = {
+      scheme: 'http',
+      host: 'example.com',
+      port: '8042',
+      authority: 'example.com:8042',
+      path: '/over/there',
+      query: 'name=ferret',
+      fragment: 'nose'
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should parse "http://example.com:8042/over/there?name=ferret&gender=male#nose" correctly', () => {
+    const result = parseURI('http://example.com:8042/over/there?name=ferret&gender=male#nose');
+    const expectedResult = {
+      scheme: 'http',
+      host: 'example.com',
+      port: '8042',
+      authority: 'example.com:8042',
+      path: '/over/there',
+      query: 'name=ferret&gender=male',
+      fragment: 'nose'
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should parse "http://example.com/over/there?name=ferret#nose" correctly', () => {
+    const result = parseURI('http://example.com/over/there?name=ferret#nose');
+    const expectedResult = {
+      scheme: 'http',
+      host: 'example.com',
+      port: undefined,
+      authority: 'example.com',
+      path: '/over/there',
+      query: 'name=ferret',
+      fragment: 'nose'
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should parse "urn:example:animal:ferret:nose" correctly', () => {
+    const result = parseURI('urn:example:animal:ferret:nose');
+    const expectedResult = {
+      scheme: 'urn',
+      host: undefined,
+      port: undefined,
+      authority: undefined,
+      path: 'example:animal:ferret:nose',
+      query: undefined,
+      fragment: undefined
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should parse "ftp://ftp.is.co.za/rfc/rfc1808.txt" correctly', () => {
+    const result = parseURI('ftp://ftp.is.co.za/rfc/rfc1808.txt');
+    const expectedResult = {
+      scheme: 'ftp',
+      host: 'ftp.is.co.za',
+      port: undefined,
+      authority: 'ftp.is.co.za',
+      path: '/rfc/rfc1808.txt',
+      query: undefined,
+      fragment: undefined
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should parse "ldap://[2001:db8::7]/c=GB?objectClass?one" correctly', () => {
+    const result = parseURI('ldap://[2001:db8::7]/c=GB?objectClass?one');
+    const expectedResult = {
+      scheme: 'ldap',
+      host: '[2001', // NA
+      port: 'db8',   // NA
+      authority: '[2001:db8::7]',
+      path: '/c=GB',
+      query: 'objectClass?one',
+      fragment: undefined
+    };
+    expect(result).to.deep.equal(expectedResult);
+  });
+});
