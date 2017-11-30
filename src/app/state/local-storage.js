24bea5fc1429ddeeeb967487502741f3b4888b5b
Leif Olsen <leif.o.olsen@gmail.com>
2017-11-30 20:22:36 +0100

    Added try/catch

--- src/app/state/local-storage.js
+++ src/app/state/local-storage.js
@@ -1,8 +1,14 @@
 /*
  * Copied from: https://github.com/marcusasplund/hyperapp-todo-simple/blob/master/src/utils/local-storage.js
  */
-const getStateFromStorage = () =>
-  JSON.parse(window.localStorage.getItem('boilerplate'));
+const getStateFromStorage = () => {
+  try {
+    return JSON.parse(window.localStorage.getItem('boilerplate'));
+  }
+  catch (e) {
+    return {};
+  }
+};
 
 const storeStateInStorage = (state) =>
   window.localStorage.setItem('boilerplate', JSON.stringify(state));
