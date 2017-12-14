dba90843b335e222217dc807c1e497ebf76961af
Leif Olsen <leif.o.olsen@gmail.com>
2017-12-14 20:44:12 +0100

    Added CSS for stroked buttons

--- src/app/views/Buttons.js
+++ src/app/views/Buttons.js
@@ -13,12 +13,21 @@ export const Buttons = () =>
       <button class={button.Button} type="button">Button</button>
       <button class={classnames(button.Button, button['Button-compact'])} type="button">Compact</button>
       <button class={classnames(button.Button, button['Button-dense'])} type="button">Dense</button>
+      <button class={button['Button-primary']} type="button">Primary</button>
       <button class={button['Button-secondary']} type="button">Secondary</button>
       <button class={button.Button} type="button" disabled>Disabled</button>
     </div>
+    <h2>Stroked Buttons</h2>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <button class={classnames(button.Button, button['Button-stroked'])} type="button">Button</button>
+      <button class={classnames(button['Button-primary'], button['Button-compact'], button['Button-stroked'])} type="button">Compact</button>
+      <button class={classnames(button['Button-secondary'], button['Button-stroked'], button['Button-dense'])} type="button">Dense</button>
+      <button class={classnames(button['Button-primary'], button['Button-stroked'])} type="button">Primary</button>
+      <button class={classnames(button['Button-secondary'], button['Button-stroked'])} type="button">Secondary</button>
+      <button class={classnames(button.Button, button['Button-stroked'])} type="button" disabled>Disabled</button>
+    </div>
     <h2>Raised Buttons</h2>
     <h2>Unelevated Buttons</h2>
-    <h2>Stroked Buttons</h2>
   </div>
 ;
 
