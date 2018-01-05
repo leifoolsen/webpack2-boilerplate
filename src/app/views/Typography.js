e956db58415fd89975731a40cc69c55b2380d840
Leif Olsen <leif.o.olsen@gmail.com>
2018-01-05 23:52:28 +0100

    Updated text

--- src/app/views/Typography.js
+++ src/app/views/Typography.js
@@ -136,12 +136,12 @@ const inlineElements = () =>
     <br/>
     <header><h2>Inline elements</h2></header>
     <p><a href="#!">This is a text link</a>.</p>
-    <p><strong>Strong is used to indicate strong importance.</strong></p>
+    <p><strong>&lt;strong&gt; is used to indicate strong importance.</strong></p>
     <p><em>This text has added emphasis.</em></p>
-    <p>The <b>b element</b> is stylistically different text from normal text,
+    <p>The <b>&lt;b&gt; element</b> is stylistically different text from normal text,
       without any special importance.</p>
-    <p>The <i>i element</i> is text that is offset from the normal text.</p>
-    <p>The <u>u element</u> is text with an unarticulated, though explicitly
+    <p>The <i>&lt;i&gt; element</i> is text that is offset from the normal text.</p>
+    <p>The <u>&lt;u&gt; element</u> is text with an unarticulated, though explicitly
       rendered, non-textual annotation.</p>
     <p>
       <del>This text is deleted</del>
@@ -159,10 +159,10 @@ const inlineElements = () =>
     <p><q cite="https://developer.mozilla.org/en-US/docs/HTML/Element/q">This
       text is a short inline quotation.</q></p>
     <p><cite>This is a citation.</cite></p>
-    <p>The <dfn>dfn element</dfn> indicates a definition.</p>
-    <p>The <mark>mark element</mark> indicates a highlight.</p>
-    <p>The <var>variable element</var>, such as <var>x</var> = <var>y</var>.</p>
-    <p>The time element:
+    <p>The <dfn>&lt;dfn&gt; element</dfn> indicates a definition.</p>
+    <p>The <mark>&lt;mark&gt; element</mark> indicates a highlight.</p>
+    <p>The <var>&lt;var&gt; element</var>, such as <var>x</var> = <var>y</var>.</p>
+    <p>The &lt;time&gt; element:&nbsp;
       <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time>
     </p>
   </section>
